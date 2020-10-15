ced7b3effd11677035c011e80f425fe8cb91a581
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-10-15 15:43:45 -0500

    added index json and markdown files

---
+++ index.js
@@ -0,0 +1,33 @@
+// it's node application
+// packagelist: inqurer || process.argv
+    // questions from the user -
+            //repo link
+            //title
+            //sections
+            //entitles descriptions
+            //table of contents
+            //installation
+            //usage
+            //license
+            //github username
+            //email
+            //tests (how to test it)
+            //questions
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
